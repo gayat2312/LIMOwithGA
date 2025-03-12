d2b7fe93f4b5456f56de5a184374bf96b62bca5f
gayat2312 <gayat23@uw.edu>
2025-03-12 03:22:37 -0700

    test_util_nodes_tf2_ros_tool.cpp

--- util_nodes_tf2_ros_tool/test/test_util_nodes_tf2_ros_tool.cpp
+++ util_nodes_tf2_ros_tool/test/test_util_nodes_tf2_ros_tool.cpp
@@ -1,46 +1,53 @@
-//google test docs
-//wiki page: https://code.google.com/p/googletest/w/list
-//primer: https://code.google.com/p/googletest/wiki/V1_7_Primer
-//FAQ: https://code.google.com/p/googletest/wiki/FAQ
-//advanced guide: https://code.google.com/p/googletest/wiki/V1_7_AdvancedGuide
-//samples: https://code.google.com/p/googletest/wiki/V1_7_Samples
+// Updated GoogleTest Documentation:
+// Official website: https://google.github.io/googletest/
+// GitHub repository: https://github.com/google/googletest
 //
-//List of some basic tests fuctions:
-//Fatal assertion                      Nonfatal assertion                   Verifies / Description
-//-------------------------------------------------------------------------------------------------------------------------------------------------------
-//ASSERT_EQ(expected, actual);         EXPECT_EQ(expected, actual);         expected == actual
-//ASSERT_NE(val1, val2);               EXPECT_NE(val1, val2);               val1 != val2
-//ASSERT_LT(val1, val2);               EXPECT_LT(val1, val2);               val1 < val2
-//ASSERT_LE(val1, val2);               EXPECT_LE(val1, val2);               val1 <= val2
-//ASSERT_GT(val1, val2);               EXPECT_GT(val1, val2);               val1 > val2
-//ASSERT_GE(val1, val2);               EXPECT_GE(val1, val2);               val1 >= val2
+// Basic assertions (fatal vs. nonfatal):
+//   ASSERT_EQ(expected, actual);     // Fails immediately if expected != actual
+//   EXPECT_EQ(expected, actual);     // Records the failure but continues running
 //
-//ASSERT_FLOAT_EQ(expected, actual);   EXPECT_FLOAT_EQ(expected, actual);   the two float values are almost equal (4 ULPs)
-//ASSERT_DOUBLE_EQ(expected, actual);  EXPECT_DOUBLE_EQ(expected, actual);  the two double values are almost equal (4 ULPs)
-//ASSERT_NEAR(val1, val2, abs_error);  EXPECT_NEAR(val1, val2, abs_error);  the difference between val1 and val2 doesn't exceed the given absolute error
+// Other useful assertions:
+//   ASSERT_NE(val1, val2);           // Asserts val1 != val2
+//   ASSERT_LT(val1, val2);           // Asserts val1 < val2
+//   ASSERT_LE(val1, val2);           // Asserts val1 <= val2
+//   ASSERT_GT(val1, val2);           // Asserts val1 > val2
+//   ASSERT_GE(val1, val2);           // Asserts val1 >= val2
 //
-//Note: more information about ULPs can be found here: http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
+// Floating-point comparisons:
+//   ASSERT_FLOAT_EQ(expected, actual); // Nearly equal floats (4 ULPs)
+//   ASSERT_DOUBLE_EQ(expected, actual); // Nearly equal doubles (4 ULPs)
+//   ASSERT_NEAR(val1, val2, abs_error);  // Absolute error threshold
 //
-//Example of two unit test:
-//TEST(Math, Add) {
-//    ASSERT_EQ(10, 5+ 5);
-//}
+// Example tests:
 //
-//TEST(Math, Float) {
-//	  ASSERT_FLOAT_EQ((10.0f + 2.0f) * 3.0f, 10.0f * 3.0f + 2.0f * 3.0f)
-//}
-//=======================================================================================================================================================
-#include "gtest/gtest.h"
+// TEST(Math, Add) {
+//     ASSERT_EQ(10, 5 + 5);
+// }
+//
+// TEST(Math, Float) {
+//     ASSERT_FLOAT_EQ((10.0f + 2.0f) * 3.0f, 10.0f * 3.0f + 2.0f * 3.0f);
+// }
+
+#include <gtest/gtest.h>
 #include <ros/ros.h>
 
-//A google test function (uncomment the next function, add code and 
-//change the names TestGroupName and TestName)
-//TEST(TestGroupName, TestName) {
-	//TODO: Add your test code here. You can use ros::NodeHandle and all other ros functionality here.
-//}
+// Sample test for integer addition
+TEST(Math, Add) {
+    ASSERT_EQ(10, 5 + 5);
+}
+
+// Sample test for floating-point arithmetic
+TEST(Math, Float) {
+    ASSERT_FLOAT_EQ((10.0f + 2.0f) * 3.0f, 10.0f * 3.0f + 2.0f * 3.0f);
+}
 
 int main(int argc, char **argv) {
+    // Initialize ROS node for tests that might use ROS functionalities.
     ros::init(argc, argv, "unittest");
-    testing::InitGoogleTest(&argc, argv);
+
+    // Initialize GoogleTest framework.
+    ::testing::InitGoogleTest(&argc, argv);
+    
+    // Run all tests and return the result.
     return RUN_ALL_TESTS();
 }
