5bfcbb6fc4f3d8853f30a06fd12b037e209598fd
gayat2312 <gayat23@uw.edu>
2025-03-12 04:31:15 -0700

    landmark_selection_scheme_random.cpp

--- keyframe_bundle_adjustment/src/landmark_selection_scheme_random.cpp
+++ keyframe_bundle_adjustment/src/landmark_selection_scheme_random.cpp
@@ -1,42 +1,37 @@
-// Copyright 2017. All rights reserved.
-// Institute of Measurement and Control Systems
-// Karlsruhe Institute of Technology, Germany
-//
-// authors:
-//  Johannes Graeter (johannes.graeter@kit.edu)
-//  and others
-
 #include "internal/landmark_selection_scheme_random.hpp"
+#include <algorithm>
+#include <random>
 
 namespace keyframe_bundle_adjustment {
 
 std::set<LandmarkId> LandmarkSparsificationSchemeRandom::getSelection(const LandmarkMap& landmarks,
-                                                                      const KeyframeMap& keyframes) const {
-
-    // copy indices from landmarks
+                                                                      const KeyframeMap& /*keyframes*/) const {
+    // Copy landmark IDs from the landmarks map.
     std::vector<LandmarkId> lm_ids;
-    lm_ids.resize(landmarks.size());
-    std::transform(landmarks.cbegin(), landmarks.cend(), lm_ids.begin(), [](const auto& a) { return a.first; });
+    lm_ids.reserve(landmarks.size());
+    std::transform(landmarks.cbegin(), landmarks.cend(), std::back_inserter(lm_ids),
+                   [](const auto& a) { return a.first; });
 
-    // shuffle them and take first num_landmarks
-    std::random_shuffle(lm_ids.begin(), lm_ids.end());
+    // Shuffle the landmark IDs using a modern random engine.
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(lm_ids.begin(), lm_ids.end(), gen);
 
+    // Select the first num_landmarks_ IDs.
     std::set<LandmarkId> selected_landmark_ids;
-    for (auto it = lm_ids.cbegin(); it != lm_ids.cend() && it != lm_ids.cbegin() + num_landmarks_; it++) {
+    auto end_it = (lm_ids.size() > num_landmarks_) ? lm_ids.begin() + num_landmarks_ : lm_ids.end();
+    for (auto it = lm_ids.cbegin(); it != end_it; ++it) {
         selected_landmark_ids.insert(*it);
     }
-
     return selected_landmark_ids;
 }
 
-
 LandmarkSparsificationSchemeBase::ConstPtr LandmarkSparsificationSchemeRandom::createConst(size_t num_landmarks) {
-
-    return LandmarkSparsificationSchemeBase::ConstPtr(new LandmarkSparsificationSchemeRandom(num_landmarks));
+    return std::make_shared<LandmarkSparsificationSchemeRandom>(num_landmarks);
 }
 
 LandmarkSparsificationSchemeBase::Ptr LandmarkSparsificationSchemeRandom::create(size_t num_landmarks) {
+    return std::make_shared<LandmarkSparsificationSchemeRandom>(num_landmarks);
+}
 
-    return LandmarkSparsificationSchemeBase::Ptr(new LandmarkSparsificationSchemeRandom(num_landmarks));
-}
-}
+} // namespace keyframe_bundle_adjustment
