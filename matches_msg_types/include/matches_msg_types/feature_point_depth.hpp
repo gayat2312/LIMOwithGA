be396cf408b3153b451817c0edf2ef4cc489e981
gayat2312 <gayat23@uw.edu>
2025-03-12 03:38:15 -0700

    feature_point_depth.hpp

--- matches_msg_types/include/matches_msg_types/feature_point_depth.hpp
+++ matches_msg_types/include/matches_msg_types/feature_point_depth.hpp
@@ -1,22 +1,20 @@
 #pragma once
-#include <Eigen/Eigen>
+#include <Eigen/Dense>
 #include "feature_point.hpp"
+
 namespace matches_msg_types {
+
 struct FeaturePointDepth : public FeaturePoint {
-    FeaturePointDepth() {
-        ;
-    }
+    // Default constructor.
+    FeaturePointDepth() = default;
 
-    FeaturePointDepth(Eigen::Vector3d p)
-	: FeaturePoint(p[0], p[1]), d(p[2]) {
-        ;
-    }
+    // Construct from an Eigen::Vector3d (explicit to avoid unintended conversions).
+    explicit FeaturePointDepth(const Eigen::Vector3d& p)
+        : FeaturePoint(static_cast<float>(p[0]), static_cast<float>(p[1]), static_cast<float>(p[2])) {}
 
+    // Construct from individual float components.
     FeaturePointDepth(float u, float v, float d)
-	: FeaturePoint(u, v), d(d) {
-        ;
-    }
-
-	float d;
+        : FeaturePoint(u, v, d) {}
 };
-}
+
+} // namespace matches_msg_types
