ba9e3a0e6df5f991843fcc392df20b2ac9ad9a67
gayat2312 <gayat23@uw.edu>
2025-03-12 04:22:54 -0700

    mono_standalone_nodelet.cpp

--- keyframe_bundle_adjustment_ros_tool/src/mono_standalone/mono_standalone_nodelet.cpp
+++ keyframe_bundle_adjustment_ros_tool/src/mono_standalone/mono_standalone_nodelet.cpp
@@ -1,18 +1,21 @@
 #include <memory>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
-
 #include "mono_standalone.hpp"
 
 namespace keyframe_bundle_adjustment_ros_tool {
 
 class MonoStandaloneNodelet : public nodelet::Nodelet {
-
-    inline void onInit() override {
+public:
+    void onInit() override {
+        // Instantiate MonoStandalone with public and private node handles.
         impl_ = std::make_unique<MonoStandalone>(getNodeHandle(), getPrivateNodeHandle());
     }
+
+private:
     std::unique_ptr<MonoStandalone> impl_;
 };
+
 } // namespace keyframe_bundle_adjustment_ros_tool
 
 PLUGINLIB_EXPORT_CLASS(keyframe_bundle_adjustment_ros_tool::MonoStandaloneNodelet, nodelet::Nodelet);
