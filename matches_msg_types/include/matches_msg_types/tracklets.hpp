904b73d148fdc8f86c874746b028da749764a4a8
gayat2312 <gayat23@uw.edu>
2025-03-12 03:39:24 -0700

    tracklets.hpp

--- matches_msg_types/include/matches_msg_types/tracklets.hpp
+++ matches_msg_types/include/matches_msg_types/tracklets.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
+#include <vector>
 #include "tracklet.hpp"
 
 namespace matches_msg_types {
 
-using TimestampNSec = uint64_t; ///< Timestamp in unix system time
+using TimestampNSec = std::uint64_t; ///< Timestamp in Unix system time (in nanoseconds)
 
+/**
+ * @brief Represents a collection of tracklets along with their corresponding timestamps.
+ */
 struct Tracklets {
-    std::vector<TimestampNSec> stamps;
-    std::vector<Tracklet> tracks;
+    std::vector<TimestampNSec> stamps; ///< Timestamps associated with each track.
+    std::vector<Tracklet> tracks;      ///< Collection of tracklets.
 };
-}
+
+} // namespace matches_msg_types
