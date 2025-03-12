62c7da73878c92e262205a19f7f0cfa3cab05e04
gayat2312 <gayat23@uw.edu>
2025-03-12 03:27:11 -0700

    static_transform_alias_nodelet.cpp

--- util_nodes_tf2_ros_tool/src/static_transform_alias/static_transform_alias_nodelet.cpp
+++ util_nodes_tf2_ros_tool/src/static_transform_alias/static_transform_alias_nodelet.cpp
@@ -1,20 +1,22 @@
 #include "static_transform_alias.hpp"
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
+#include <boost/make_shared.hpp>
 
 namespace util_nodes_tf2_ros_tool {
 
 class StaticTransformAliasNodelet : public nodelet::Nodelet {
-
-    virtual void onInit();
+public:
+    virtual void onInit() override;
+private:
     boost::shared_ptr<StaticTransformAlias> m_;
 };
 
 void StaticTransformAliasNodelet::onInit() {
-    m_.reset(new StaticTransformAlias(getNodeHandle(), getPrivateNodeHandle()));
+    // Create a shared pointer to the StaticTransformAlias using the node handles provided by the nodelet.
+    m_ = boost::make_shared<StaticTransformAlias>(getNodeHandle(), getPrivateNodeHandle());
 }
 
 } // namespace util_nodes_tf2_ros_tool
 
-PLUGINLIB_DECLARE_CLASS(util_nodes_tf2_ros_tool, StaticTransformAliasNodelet,
-                        util_nodes_tf2_ros_tool::StaticTransformAliasNodelet, nodelet::Nodelet);
+PLUGINLIB_EXPORT_CLASS(util_nodes_tf2_ros_tool::StaticTransformAliasNodelet, nodelet::Nodelet);
