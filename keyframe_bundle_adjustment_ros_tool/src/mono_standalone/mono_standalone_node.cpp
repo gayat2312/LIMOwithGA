ece9d5a46a155ca2e9162cf51b828af3cab6d412
gayat2312 <gayat23@uw.edu>
2025-03-12 04:21:34 -0700

    mono_standalone_node.cpp

--- keyframe_bundle_adjustment_ros_tool/src/mono_standalone/mono_standalone_node.cpp
+++ keyframe_bundle_adjustment_ros_tool/src/mono_standalone/mono_standalone_node.cpp
@@ -1,11 +1,19 @@
 #include "mono_standalone.hpp"
+#include <ros/ros.h>
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char* argv[])
+{
+    // Initialize ROS node with the name "mono_standalone_node"
     ros::init(argc, argv, "mono_standalone_node");
 
-    keyframe_bundle_adjustment_ros_tool::MonoStandalone mono_standalone(ros::NodeHandle(), ros::NodeHandle("~"));
+    // Create public and private node handles.
+    ros::NodeHandle nh;
+    ros::NodeHandle nh_private("~");
 
+    // Instantiate the MonoStandalone object.
+    keyframe_bundle_adjustment_ros_tool::MonoStandalone mono_standalone(nh, nh_private);
+
+    // Enter the ROS event loop.
     ros::spin();
     return EXIT_SUCCESS;
 }
