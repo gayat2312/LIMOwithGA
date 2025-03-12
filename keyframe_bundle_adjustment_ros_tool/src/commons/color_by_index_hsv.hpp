046f6eab2ae9ac7540d7b86858e5315789c2d2e5
gayat2312 <gayat23@uw.edu>
2025-03-12 03:48:00 -0700

    color_by_index_hsv.hpp

--- keyframe_bundle_adjustment_ros_tool/src/commons/color_by_index_hsv.hpp
+++ keyframe_bundle_adjustment_ros_tool/src/commons/color_by_index_hsv.hpp
@@ -1,51 +1,60 @@
-/*
- * Copyright 2014. All rights reserved.
- * Institute of Measurement and Control Systems
- * Karlsruhe Institute of Technology, Germany
- *
- * authors:
- *  Johannes Graeter (johannes.graeter@kit.edu)
- *  and others
- */
-#ifndef COLORBYINDEX_HPP
-#define COLORBYINDEX_HPP
+#pragma once
 
 #include <opencv2/opencv.hpp>
+#include <cstdint>
 
 namespace util_image {
-/// /////////////////////////////////////////////
-/// @brief function to convert color in hsv to bgr
-/// @return color in bgr
-/// @par color in hsv
-///
-/// uses the intern opencv color representation, so max s,v = 255 max(h)=180 (increase of one means
-/// increase of 2°)
-inline cv::Scalar hsv2bgr(cv::Scalar ColorHSV) {
-    cv::Mat old_color(1, 1, CV_8UC3, ColorHSV);
-    cv::Mat new_color(1, 1, CV_8UC3);
-    cv::cvtColor(old_color, new_color, CV_HSV2BGR);
-    return cv::Scalar(new_color.at<cv::Vec3b>(0, 0));
+
+/**
+ * @brief Convert a color from HSV to BGR.
+ *
+ * The input color is in the OpenCV HSV format:
+ * - H: [0, 180] (each unit represents 2°)
+ * - S and V: [0, 255]
+ *
+ * @param colorHSV Input color in HSV.
+ * @return cv::Scalar The corresponding color in BGR.
+ */
+inline cv::Scalar hsv2bgr(const cv::Scalar& colorHSV) {
+    cv::Mat hsvMat(1, 1, CV_8UC3, colorHSV);
+    cv::Mat bgrMat;
+    cv::cvtColor(hsvMat, bgrMat, cv::COLOR_HSV2BGR);
+    return cv::Scalar(bgrMat.at<cv::Vec3b>(0, 0));
 }
-inline cv::Scalar get_color(uint32_t ID, int NumColors) {
+
+/**
+ * @brief Get a color based on an ID and a total number of colors.
+ *
+ * If ID is 0, a default color is returned. Otherwise, the function computes an HSV value
+ * with the hue determined by the modulo of the (ID - 1) over the number of colors, and converts it to BGR.
+ *
+ * @param ID Identifier used to generate the color.
+ * @param numColors Total number of distinct colors.
+ * @return cv::Scalar The computed BGR color.
+ */
+inline cv::Scalar get_color(uint32_t ID, int numColors) {
     if (ID == 0) {
         return cv::Scalar(123, 22, 234);
     } else {
-        uint32_t ModID = ID - 1;
-        ModID = ModID % NumColors;
-
-        int dH = 180 / NumColors;
+        uint32_t modID = (ID - 1) % static_cast<uint32_t>(numColors);
+        int dH = 180 / numColors;
         int S = 200;
         int V = 200;
-
-        return hsv2bgr(cv::Scalar(ModID * dH, S, V));
+        return hsv2bgr(cv::Scalar(modID * dH, S, V));
     }
 }
 
+/**
+ * @brief Get a color based solely on an ID.
+ *
+ * A seeded random generator ensures that the same ID always results in the same color.
+ *
+ * @param ID Identifier used to generate the color.
+ * @return cv::Scalar A randomly generated BGR color.
+ */
 inline cv::Scalar get_color(uint32_t ID) {
-    // seed generator with ID -> always same color
     cv::RNG rng(ID);
-    // Generate number from uniform distribution
-    return cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+    return cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 }
-}
-#endif // COLORBYINDEX_HPP
+
+} // namespace util_image
