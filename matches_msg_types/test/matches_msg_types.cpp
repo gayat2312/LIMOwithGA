c913cb049c868e19b3065799b0187bcbb31d0f61
gayat2312 <gayat23@uw.edu>
2025-03-12 03:36:37 -0700

    matches_msg_types.cpp

--- matches_msg_types/test/matches_msg_types.cpp
+++ matches_msg_types/test/matches_msg_types.cpp
@@ -1,40 +1,34 @@
-//google test docs
-//wiki page: https://code.google.com/p/googletest/w/list
-//primer: https://code.google.com/p/googletest/wiki/V1_7_Primer
-//FAQ: https://code.google.com/p/googletest/wiki/FAQ
-//advanced guide: https://code.google.com/p/googletest/wiki/V1_7_AdvancedGuide
-//samples: https://code.google.com/p/googletest/wiki/V1_7_Samples
+// Updated GoogleTest Documentation:
+// Official website: https://google.github.io/googletest/
+// GitHub repository: https://github.com/google/googletest
 //
-//List of some basic tests fuctions:
-//Fatal assertion                      Nonfatal assertion                   Verifies / Description
-//-------------------------------------------------------------------------------------------------------------------------------------------------------
-//ASSERT_EQ(expected, actual);         EXPECT_EQ(expected, actual);         expected == actual
-//ASSERT_NE(val1, val2);               EXPECT_NE(val1, val2);               val1 != val2
-//ASSERT_LT(val1, val2);               EXPECT_LT(val1, val2);               val1 < val2
-//ASSERT_LE(val1, val2);               EXPECT_LE(val1, val2);               val1 <= val2
-//ASSERT_GT(val1, val2);               EXPECT_GT(val1, val2);               val1 > val2
-//ASSERT_GE(val1, val2);               EXPECT_GE(val1, val2);               val1 >= val2
+// Basic assertions:
+//   ASSERT_EQ(expected, actual);     // Fatal assertion: test stops if failure occurs.
+//   EXPECT_EQ(expected, actual);     // Non-fatal assertion: test continues on failure.
 //
-//ASSERT_FLOAT_EQ(expected, actual);   EXPECT_FLOAT_EQ(expected, actual);   the two float values are almost equal (4 ULPs)
-//ASSERT_DOUBLE_EQ(expected, actual);  EXPECT_DOUBLE_EQ(expected, actual);  the two double values are almost equal (4 ULPs)
-//ASSERT_NEAR(val1, val2, abs_error);  EXPECT_NEAR(val1, val2, abs_error);  the difference between val1 and val2 doesn't exceed the given absolute error
+// Floating-point comparisons:
+//   ASSERT_FLOAT_EQ(expected, actual); // Compares floats (within 4 ULPs)
+//   ASSERT_DOUBLE_EQ(expected, actual);// Compares doubles (within 4 ULPs)
+//   ASSERT_NEAR(val1, val2, abs_error);  // Verifies that the difference does not exceed abs_error
 //
-//Note: more information about ULPs can be found here: http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
-//
-//Example of two unit test:
-//TEST(Math, Add) {
-//    ASSERT_EQ(10, 5+ 5);
-//}
-//
-//TEST(Math, Float) {
-//	  ASSERT_FLOAT_EQ((10.0f + 2.0f) * 3.0f, 10.0f * 3.0f + 2.0f * 3.0f)
-//}
-//=======================================================================================================================================================
+// Example tests:
+
 #include "gtest/gtest.h"
 
-//A google test function (uncomment the next function, add code and 
-//change the names TestGroupName and TestName)
-//TEST(TestGroupName, TestName) {
-	//TODO: Add your test code here
-//}
+TEST(Math, Add) {
+    // Verifies that addition is performed correctly.
+    ASSERT_EQ(10, 5 + 5);
+}
 
+TEST(Math, Float) {
+    // Verifies that floating point arithmetic is computed as expected.
+    ASSERT_FLOAT_EQ((10.0f + 2.0f) * 3.0f, 10.0f * 3.0f + 2.0f * 3.0f);
+}
+
+// Note: The GoogleTest framework provides a main() function by default.
+// If you wish to define your own main, you can uncomment the code below:
+//
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
