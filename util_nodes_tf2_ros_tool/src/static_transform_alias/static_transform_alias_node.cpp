5bb8e184f2ce9fe1bf40b8e78c4049e39963cbdd
gayat2312 <gayat23@uw.edu>
2025-03-12 03:25:57 -0700

    static_transform_alias_node.cpp

--- util_nodes_tf2_ros_tool/src/static_transform_alias/static_transform_alias_node.cpp
+++ util_nodes_tf2_ros_tool/src/static_transform_alias/static_transform_alias_node.cpp
@@ -1,11 +1,18 @@
 #include "static_transform_alias.hpp"
+#include <ros/ros.h>
 
 int main(int argc, char* argv[]) {
-
+    // Initialize the ROS node with a specified name.
     ros::init(argc, argv, "static_transform_alias_node");
 
-    util_nodes_tf2_ros_tool::StaticTransformAlias static_transform_alias(ros::NodeHandle(), ros::NodeHandle("~"));
+    // Create node handles: one for public and one for private parameters.
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
 
+    // Instantiate the StaticTransformAlias object.
+    util_nodes_tf2_ros_tool::StaticTransformAlias static_transform_alias(nh, private_nh);
+
+    // Enter the ROS event loop.
     ros::spin();
     return 0;
 }
